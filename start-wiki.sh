b1aad5e08997e5280ef89470fe717a70d3a6c377
PastaMasta <pasta.masta2902@gmail.com>
2022-08-12 15:00:22 +0100

    Switched to no edit and use the current branch as base

--- start-wiki.sh
+++ start-wiki.sh
@@ -2,7 +2,10 @@
 [[ -n ${DEBUG} ]] && set -x
 
 docker-compose down
-docker-compose up --detach
+
+export REF=$(git branch --show-current)
+echo "Ref is: $REF"
+docker-compose up --detach --always-recreate-deps --renew-anon-volumes
 
 if [[ -n ${WSL_DISTRO_NAME} ]] ; then
   cmd.exe /C start http://127.0.0.1:4567
